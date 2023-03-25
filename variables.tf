23ee4c5793e5127fb098e9c83c1701b78c86177b
kengo922 <kengo.matsuno.922@gmail.com>
2023-03-25 21:18:56 +0900

    feat: variables.tfの変数情報を修正

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
 # 変数の設定
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region"{}
-variable "profile"{}
+variable "aws_access_key" {
+    description = "AWSのアクセスキーを設定する"
+    type        = string
+}
+variable "aws_secret_key" {
+    description = "AWSのシークレットキーを設定する"
+    type        = string
+}
+variable "region"{
+    description = "リージョンを設定する"
+    type        = string
+}
+variable "profile"{
+    description = "profileを設定する"
+    type        = string
+}
